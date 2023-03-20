c59cd6788d03a6207c85f90e6413ccb4ef36004c
Aylin Morales <aylinmorales@Aylins-MacBook-Pro.local>
2023-03-20 12:50:11 -0700

    index.js added

---
+++ index.js
@@ -0,0 +1,40 @@
+const express= require('express');
+const session= require('express-session');
+const exphbs= require('express-handlebars');
+const path= require('path');
+const SequelizeStore=require('connect-session-sequelize')(session.Store);
+const allRoutes= require('./controllers');
+
+const sequelize= require('./config/connection');
+
+const app= express();
+const PORT= process.env.PORT || 3002;
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+      maxAge:1000*60*60*2
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize
+  })
+};
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+//handlebars
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(allRoutes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
