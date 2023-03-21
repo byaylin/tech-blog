b75ce5d52cdb43c202ca371b8a21c0f35dd58d4f
byaylin <aylinxmorales@gmail.com>
2023-03-21 13:56:31 -0700

    updates for heroku deploy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const sequelize= require('./config/connection');
 const app= express();
 const PORT= process.env.PORT || 3002;
 const sess = {
-  secret: process.env.SESSION_SECRET,
+  secret: "hi",
   cookie: {
       maxAge:1000*60*60*2
   },
